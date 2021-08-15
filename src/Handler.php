4251510939831ea245cd2d210bcb5f580083abb0
Matthias Larsen <matthias@msalconsulting.dk>
2021-08-15 11:48:04 +0200

    fix: channel_data encoding when verifying auth subscription

--- src/Handler.php
+++ src/Handler.php
@@ -93,7 +93,7 @@ class Handler extends WebsocketHandler
             $data = "{$event->getConnectionId()}:{$channel}";
 
             if ($channelData) {
-                $data .= ':' . json_encode($channelData);
+                $data .= ':' . json_encode(json_decode($channelData, true));
             }
 
             $signature = hash_hmac('sha256', $data, config('app.key'), false);
